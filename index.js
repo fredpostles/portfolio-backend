da4e7d43dcbeed432caae44e8d8647346aa1c1a5
Your Name <fred.postles@gmail.com>
2023-04-29 01:41:16 +0100

    cors fix

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors(corsOptions));
 // middleware
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
