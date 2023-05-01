1d597904a09621d8932ed014c1055caa30b0d12a
Your Name <fred.postles@gmail.com>
2023-05-01 12:28:08 +0100

    CORS edit

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const app = express();
 const { db } = require("./mongoose/connection"); //import the connection
 const cors = require("cors");
 
-const corsOptions = {
-  origin: process.env.APP_URL,
-};
+// const corsOptions = {
+//   origin: process.env.APP_URL,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // middleware
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.APP_URL);
+  res.setHeader("Access-Control-Allow-Origin", `${process.env.APP_URL}`);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
